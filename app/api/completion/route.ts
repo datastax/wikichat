3d46a4ebcd1539dd51b8eb6e9f3c04dcc313f24a
Mike Fortman <michael.fortman@datastax.com>
2023-12-21 15:25:13 -0600

    add log

--- app/api/completion/route.ts
+++ app/api/completion/route.ts
@@ -25,48 +25,48 @@ export async function POST(req: Request) {
   try {
     let docContext = '';
 
-    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    // const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    const currentDate = new Date();
-    const day = currentDate.getDate();
-    const month = months[currentDate.getMonth()];
-    const year = currentDate.getFullYear();
+    // const currentDate = new Date();
+    // const day = currentDate.getDate();
+    // const month = months[currentDate.getMonth()];
+    // const year = currentDate.getFullYear();
     
-    // add the appropriate suffix to the day
-    const getDayWithSuffix = (day) => {
-      if (day >= 11 && day <= 13) {
-        return day + "th";
-      }
-      switch (day % 10) {
-        case 1:
-          return day + "st";
-        case 2:
-          return day + "nd";
-        case 3:
-          return day + "rd";
-        default:
-          return day + "th";
-      }
-    }
+    // // add the appropriate suffix to the day
+    // const getDayWithSuffix = (day) => {
+    //   if (day >= 11 && day <= 13) {
+    //     return day + "th";
+    //   }
+    //   switch (day % 10) {
+    //     case 1:
+    //       return day + "st";
+    //     case 2:
+    //       return day + "nd";
+    //     case 3:
+    //       return day + "rd";
+    //     default:
+    //       return day + "th";
+    //   }
+    // }
     
-    const formattedDate = `${month} ${getDayWithSuffix(day)} ${year}`;
+    // const formattedDate = `${month} ${getDayWithSuffix(day)} ${year}`;
 
-    const embedded = await cohere.embed({
-      texts:[formattedDate],
-      model: "embed-english-light-v3.0",
-      inputType: "search_query",
-    });
+    // const embedded = await cohere.embed({
+    //   texts:[formattedDate],
+    //   model: "embed-english-light-v3.0",
+    //   inputType: "search_query",
+    // });
 
     try {
-      const collection = await astraDb.collection(ASTRA_DB_COLLECTION);
+      // const collection = await astraDb.collection(ASTRA_DB_COLLECTION);
       const metadataCollection = await astraDb.collection("article_metadata");
 
-      const cursor = collection.find(null, {
-        sort: {
-          $vector: embedded?.embeddings[0],
-        },
-        limit: 20,
-      });
+      // const cursor = collection.find(null, {
+      //   sort: {
+      //     $vector: embedded?.embeddings[0],
+      //   },
+      //   limit: 20,
+      // });
 
       const metadataCursor = metadataCollection.find({},
         {
@@ -78,9 +78,9 @@ export async function POST(req: Request) {
           limit: 3
         });
 
-      const documents = await cursor.toArray();
+      // const documents = await cursor.toArray();
 
-      const docsMap = documents?.map(doc => { return {title: doc.title, context: doc.content }});
+      // const docsMap = documents?.map(doc => { return {title: doc.title, context: doc.content }});
 
       const metadataDocuments = await metadataCursor.toArray();
 
@@ -91,6 +91,8 @@ export async function POST(req: Request) {
         }
       });
 
+      console.log(metadataDocsMap);
+      
       docContext = JSON.stringify(metadataDocsMap);
 
     } catch (e) {
