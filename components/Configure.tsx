8524a6acd883a3b82178f0915a2f2bb253b111e3
Mike Fortman <michael.fortman@datastax.com>
2024-01-10 10:46:56 -0600

    add new model

--- components/Configure.tsx
+++ components/Configure.tsx
@@ -22,14 +22,8 @@ const Configure = ({ isOpen, onClose, useRag, llm, similarityMetric, setConfigur
 
   const llmOptions = [
     { label: 'GPT 3.5 Turbo', value: 'gpt-3.5-turbo' },
-    { label: 'GPT 4', value: 'gpt-4' }
-    // { label: 'Meta: Llama 2 Chat', value: 'meta.llama2-13b-chat-v1' },
-    // { label: 'AI21 Labs: Jurassic-2 Mid', value: 'ai21.j2-mid-v1' },
-    // { label: 'AI21 Labs: Jurassic-2 Ultra', value: 'ai21.j2-ultra-v1' },
-    // { label: 'Anthropic Claude v2', value: 'anthropic.claude-v2'},
-    // { label: 'Anthropic Claude v1', value: 'anthropic.claude-v1'}
-    // { label: 'Cohere: Command', value: 'cohere.command-text-v14' },
-    // { label: 'Cohere: Command Light', value: 'cohere.command-light-text-v14' },
+    { label: 'GPT 4', value: 'gpt-4' },
+    { label: 'GPT 4 Turbo', value: 'gpt-4-1106-preview' },
   ];
 
   const similarityMetricOptions = [
@@ -68,7 +62,7 @@ const Configure = ({ isOpen, onClose, useRag, llm, similarityMetric, setConfigur
               value={selectedLlm}
               onSelect={setSelectedLlm}
             />
-            <Toggle enabled={rag} label="Enable vector content (RAG)" onChange={() => setRag(!rag)} />
+            {/* <Toggle enabled={rag} label="Enable vector content (RAG)" onChange={() => setRag(!rag)} /> */}
           </div>
           {/* <Dropdown
             fieldId="similarityMetric"
